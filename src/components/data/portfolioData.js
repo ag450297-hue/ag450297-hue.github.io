b4e5741545787f64b3971ba80cfd128fdd515809
AngelicaG28 <ag450297@gmail.com>
2026-02-14 01:39:22 -0500

    PROY6

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -9,3 +9,21 @@ export const portfolioData = [
     anim: 'fade-up',
   }
 ];
+
+const skillIcons = {
+  JavaScript: 'skill-icons:javascript',
+  React: 'skill-icons:react-dark',
+  Astro: 'skill-icons:astro',
+  CSS: 'skill-icons:css',
+  Sass: 'skill-icons:sass',
+  StyledComponents: 'skill-icons:styledcomponents',
+  Bootstrap: 'skill-icons:bootstrap',
+  Tailwind: 'skill-icons:tailwindcss-dark',
+};
+
+export const getPortafolioData = portfolioData.map((item) => {
+  return {
+    ...item,
+    skills: item.skills.map((skill) => skillIcons[skill]),
+  };
+});
