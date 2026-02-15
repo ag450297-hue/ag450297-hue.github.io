248c2e277cd4c27fbe2c3f7fd4a9e5ad928a1ff4
AngelicaG28 <ag450297@gmail.com>
2026-02-15 15:42:33 -0500

    arr

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -2,12 +2,11 @@ export const mainMenu = [
      {link:"#", content:"Home", icon:"lucide:home"},
      {link:"/presentacion", content:"Presentación", icon:"mdi:account-video"},
      {link:"#about", content:"Sobre mí", icon:"octicon:person-24"},
-     {link:"#portafolio", content:"Evidencias", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Blog", icon:"mdi:blog"}
+     {link:"#evidencias", content:"Evidencias", icon:"carbon:portfolio"},
 ]
 
 export const menuBlog = [
      {link:"/", content:"Home", icon:"lucide:home"},
      {link:"/#about", content:"Sobre mí", icon:"octicon:person-24"},
-     {link:"/#portafolio", content:"Evidencias", icon:"carbon:portfolio"},
+     {link:"/#evidencias", content:"Evidencias", icon:"carbon:portfolio"},
 ]
